acabf302324da67a2f9d87d577be09ffca0982f0
Maria Emilia Bergo <mariaemilia.bergo@gmail.com>
2021-02-22 21:03:19 -0300

    Initial commit

---
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+
+const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.post('/', appointmentsController.create);
+
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
+
+//   return response.json(appointments);
+// });
+
+export default appointmentsRouter;
